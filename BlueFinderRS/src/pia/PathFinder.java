b5c9a7928b7bcc0ac8ac5f1046104523d131a063
yukamustang <yuka.mustang@gmail.com>
2013-07-19 20:15:19 -0300

    path finder test

--- BlueFinderRS/src/pia/PathFinder.java
+++ BlueFinderRS/src/pia/PathFinder.java
@@ -15,6 +15,10 @@ import java.util.logging.Logger;
 import normalization.BasicNormalization;
 import normalization.INormalizator;
 import db.WikipediaConnector;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  *
@@ -34,6 +38,23 @@ public class PathFinder {
     private int regularGeneratedPaths = 0;
     private List<String> analysedPathQueryRetrieved; 
     private INormalizator normalizator;
+    private static final List<String> BLACKLIST_CATEGORY;
+    static {
+        List<String> tmp = new ArrayList<String>();
+        try {
+            FileReader fileReader = new FileReader("blackilist_category.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                tmp.add(line);
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            System.err.print("Some error ocurred while loading category's blacklist.");
+            e.printStackTrace();
+        }
+        BLACKLIST_CATEGORY = Collections.unmodifiableList(tmp);
+    }
 
     public PathFinder() {
         this.visited = new HashSet<Integer>();
@@ -82,7 +103,6 @@ public class PathFinder {
         this.iterations = 0;
         this.reason = "";
 
-
         this.visited = new HashSet<Integer>();
         Set<Integer> initialSet = new HashSet<Integer>();
 
@@ -124,7 +144,6 @@ public class PathFinder {
             this.iterations += 1;
             return this.findPath(toContinue, pageIdTo);
         }
-
         System.out.println("Es vacio");
         this.reason = "Path does not exists";
         return false;
@@ -150,13 +169,10 @@ public class PathFinder {
                 Integer idDestination = rs.getInt("pageid");
                 result.add(idDestination);
                 //    System.out.println("New Destination: "+ idDestination);
-
             }
             rs.close();
-
         }
         //wikipediaConnection.close();
-
         return result;
     }
 
@@ -178,7 +194,6 @@ public class PathFinder {
                // System.out.println("Camino encontrado para " + person);
             } else {
                 //System.out.println("INSERT INTO not_found_path (from, to, hops, reason) VALUES (\"" + person + "\",\"" + city + "\",\"" + finder.getIterations() + "\",\"" + finder.getReason() + "\" )");
-
                 st2.executeUpdate("INSERT INTO not_found_path (page_from, page_to, hops, reason) VALUES (\"" + person + "\",\"" + city + "\",\"" + finder.getIterations() + "\",\"" + finder.getReason() + "\" )");
                 //System.out.println("Camino NO para " + person);
             }
@@ -190,7 +205,6 @@ public class PathFinder {
     private Integer getPageId(String from) throws ClassNotFoundException {
         int page = 0;
         try {
-            
             Connection c = WikipediaConnector.getConnection();
             Statement st = c.createStatement();
           //  System.out.println("Select page_id from page where page_namespace=0 and page_title=\"" + from + "\"");
@@ -212,7 +226,6 @@ public class PathFinder {
     private Integer getCatPageId(String from) throws ClassNotFoundException {
         int page = 0;
         try {
-            
             Connection c = WikipediaConnector.getConnection();
             Statement st = c.createStatement();
             //System.out.println("Select page_id from page where page_namespace=14 and page_title=\"" + from + "\"");
@@ -229,9 +242,10 @@ public class PathFinder {
         } catch (SQLException ex) {
            // System.out.println("Error para obtener el id de "+from);
            // Logger.getLogger(PathFinder.class.getName()).log(Level.SEVERE, from, ex);
-        }finally {return page;}
+        } finally {
+            return page;
+        }
        // System.out.println("Luego del catch de error"+from);
-        
     }
 
     public String getReason() {
@@ -265,7 +279,6 @@ public class PathFinder {
 
         this.specificPaths = allPaths;
         return this.specificPaths;
-
     }
 
     /**
@@ -280,18 +293,16 @@ public class PathFinder {
         Connection c = WikipediaConnector.getConnection();
         Statement st = c.createStatement();
         String query_text = "SELECT convert(cl_to using utf8) as cl_to FROM categorylinks c where cl_from=" + pageId + " and cl_type=\"page\"";
-       // System.out.println(query_text);
-
+       
         ResultSet rs = st.executeQuery(query_text);
 
         while (rs.next()) {
-            String catTo = rs.getString("cL_to");
+            String catTo = rs.getString("cl_to");
             //InputStream stream = rs.getBinaryStream("cl_to");
             //String catTo = stream.toString();
-            if (!this.categoryBlackList().contains(catTo)) {
+            if (!BLACKLIST_CATEGORY.contains(catTo)) {
                 listCategories.add(catTo);
             }
-
         }
         st.close();
        // c.close();
@@ -299,8 +310,7 @@ public class PathFinder {
     }
     
     String getStringValue(byte[] varbinary){
-             
-               return  new String(varbinary);
+        return  new String(varbinary);
     }
 
     /**
@@ -317,16 +327,16 @@ public class PathFinder {
             this.regularGeneratedPaths++; //all paths generated counter.
             currentPath.remove(currentPath.size() - 1);
         } 
-            if (this.categoryPathIterations < this.catIterationsLevel) {
-                List<String> subCategories = this.getSubCategories(categoryName);
-                for (String subCategoryName : subCategories) {
-                    this.categoryPathIterations++;
-                    currentPath.add(this.normalizeCategory(subCategoryName, fromPage,toPage));
-                    this.getPathUsingCategories(subCategoryName,fromPage, toPage, currentPath, allPaths);
-                    currentPath.remove(currentPath.size() - 1);
-                    this.categoryPathIterations--;
-                }
+        if (this.categoryPathIterations < this.catIterationsLevel) {
+            List<String> subCategories = this.getSubCategories(categoryName);
+            for (String subCategoryName : subCategories) {
+                this.categoryPathIterations++;
+                currentPath.add(this.normalizeCategory(subCategoryName, fromPage,toPage));
+                this.getPathUsingCategories(subCategoryName,fromPage, toPage, currentPath, allPaths);
+                currentPath.remove(currentPath.size() - 1);
+                this.categoryPathIterations--;
             }
+        }
     }
 
     /**
@@ -358,7 +368,7 @@ public class PathFinder {
      */
     private List<String> getSubCategories(String categoryName) throws ClassNotFoundException {
         Integer catPageId = this.getCatPageId(categoryName);
-            List<String> listCategories = new ArrayList<String>();
+        List<String> listCategories = new ArrayList<String>();
         try {
             
           //  System.out.println("Gettin subcategories from: " + catName.toUpperCase());
@@ -395,26 +405,6 @@ public class PathFinder {
         return this.normalizator.normalizeCategory(subCategoryName, fromCatName, toCatName);
     }
 
-    private List<String> categoryBlackList() {
-        List<String> list = new ArrayList<String>();
-
-        list.add("All_articles_needing_additional_references");
-        list.add("All_articles_with_dead_external_links");
-        list.add("All_articles_with_unsourced_statements");
-        list.add("Articles_containing_explicitly_cited_English_language_text");
-        list.add("Articles_needing_additional_references_from_December_2010");
-        list.add("Articles_with_Spanish_language_external_links");
-        list.add("Articles_with_dead_external_links_from_September_2010");
-        list.add("Articles_containing_Latin_language_text");
-        list.add("All_articles_needing_cleanup");
-        list.add("Articles_with_dead_external_links_from_October_2010");
-        list.add("Articles_with_unsourced_statements_from_May_2010");
-        list.add("All_articles_to_be_expanded");
-
-
-        return list;
-    }
-
     /**
      * Return the number of relevantDocuments for the query path.
      * @param queryPathList
@@ -425,65 +415,61 @@ public class PathFinder {
         String query = "SELECT distinct up.page FROM U_page up";
         String query_notfound = "SELECT v_from, u_to FROM NFPC";
         
-          
-          Connection dbresearch = WikipediaConnector.getResultsConnection();
-          
-          Statement st = dbresearch.createStatement();
-          
-          ResultSet normalizedPaths = st.executeQuery(query);
-          
-          while (normalizedPaths.next()) {            
-              //String path = normalizedPaths.getString("path");
-              String page = normalizedPaths.getString("page");
-              
-              String from = this.getFrom(page);
-              String to = this.getTo(page);
-             
-              this.computeRelevantDocuments(pathQuery,from,to);
-          }
-          
-          normalizedPaths = st.executeQuery(query_notfound);
-          while (normalizedPaths.next()) {            
-              //String path = normalizedPaths.getString("path");
-              //String page = normalizedPaths.getString("page");
-              
-              String from = normalizedPaths.getString("v_from");
-              String to = normalizedPaths.getString("u_to");
-             
-              this.computeRelevantDocuments(pathQuery,from,to);
-          }
-          
-          st.close();
-          
-          return 0;
+        Connection dbresearch = WikipediaConnector.getResultsConnection();
+
+        Statement st = dbresearch.createStatement();
+
+        ResultSet normalizedPaths = st.executeQuery(query);
+
+        while (normalizedPaths.next()) {            
+            //String path = normalizedPaths.getString("path");
+            String page = normalizedPaths.getString("page");
+
+            String from = this.getFrom(page);
+            String to = this.getTo(page);
+
+            this.computeRelevantDocuments(pathQuery,from,to);
+        }
+
+        normalizedPaths = st.executeQuery(query_notfound);
+        while (normalizedPaths.next()) {            
+            //String path = normalizedPaths.getString("path");
+            //String page = normalizedPaths.getString("page");
+
+            String from = normalizedPaths.getString("v_from");
+            String to = normalizedPaths.getString("u_to");
+
+            this.computeRelevantDocuments(pathQuery,from,to);
+        }
+
+        st.close();  
+        return 0;
     }
     
     /*
      * Algoritmo usado para calcular el recall y precision 
      */
     private void computeRelevantDocuments(String path, String from, String to) throws ClassNotFoundException, SQLException, UnsupportedEncodingException{
-        
         if(this.isReachablePath(path,from,to)){
-        //    System.out.println("Is reachable "+ path + " from " + from +" to " + to);
-        String categoryName = this.getCategory(path,from, to);
+            //    System.out.println("Is reachable "+ path + " from " + from +" to " + to);
+            String categoryName = this.getCategory(path,from, to);
         
-        if(!this.analysedPathQueryRetrieved.contains(categoryName)){
-        
-        if(!categoryName.equals("")){
-            this.analysedPathQueryRetrieved.add(categoryName);
-            this.writeRelevantPagesFromCategory(categoryName);
-        }else{
-            
-            this.analysedPathQueryRetrieved.add("DIEGO"+from);
-            this.writeRelevantPagesFromDirectLink(from);
-        }}}
+            if(!this.analysedPathQueryRetrieved.contains(categoryName)) {
+                if(!categoryName.equals("")) {
+                    this.analysedPathQueryRetrieved.add(categoryName);
+                    this.writeRelevantPagesFromCategory(categoryName);
+                } else {
+                    this.analysedPathQueryRetrieved.add("DIEGO"+from);
+                    this.writeRelevantPagesFromDirectLink(from);
+                }
+            }
+        }
     }
     
     /*
      * Algoritmo viejo, creo que puede ser borrado.
      */
     public boolean isReachablePath(String pathQuery, String from, String to) throws ClassNotFoundException, SQLException, UnsupportedEncodingException{
-        
         if(pathQuery.equals("[to]")){
             return true;
         }
@@ -492,39 +478,31 @@ public class PathFinder {
         for (int i = 0; i < steps.length; i++) {
             steps[i] = steps[i].replaceAll("\\[from\\]", from);
             steps[i] = steps[i].replaceAll("\\[to\\]", to);
-        }
-        
+        }    
          
-                 if(this.getCategoriesFromPage(from).contains(steps[0])){
-                     
-                     return this.pathReacheableCategories(steps);
-                 }else return false;
-            
-            
-        
-      }
+        if(this.getCategoriesFromPage(from).contains(steps[0])) {   
+            return this.pathReacheableCategories(steps);
+        } else { 
+            return false;
+        }
+    }
     
     private boolean pathReacheableCategories(String[] steps) throws ClassNotFoundException{
         for (int i = 0; i < steps.length-1; i++) {
             if(!this.getSubCategories(steps[i]).contains(steps[i+1])){
-              
                 return false;
-            }
-           
+            }           
         }
         return true;
     }
     
     private void writeRelevantPagesFromDirectLink(String from){
         try {
-           
             int pageId = this.getPageId(from);
             Connection wikipedia = WikipediaConnector.getConnection();
             Statement st = wikipedia.createStatement();
             
-            
-            
-            if(pageId!=0){
+            if(pageId != 0){
                 Set<Integer> nodes = new HashSet<Integer>();
                 nodes.add(pageId);
                 List<Integer> res = this.getDirectNodesFrom(nodes);
@@ -537,13 +515,11 @@ public class PathFinder {
                       String page_title = this.getStringValue(varbinary);
                     //String page_title = rs.getString("page_title");
                     this.saveRelatedPage(page_title);
-                    }
-                    
+                    }   
                 }
-                 
-            
-            }else{
-                System.out.println("from:" + from +" no tiene page id");}
+            } else {
+                System.out.println("from:" + from +" no tiene page id");
+            }
             st.close();
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(PathFinder.class.getName()).log(Level.SEVERE, null, ex);
@@ -559,8 +535,7 @@ public class PathFinder {
             Statement st = wikipedia.createStatement();
             
             String query_text = "SELECT p.page_title FROM categorylinks c, page p where cl_to=\""+categoryName+"\" and cl_type=\"page\" and p.page_id=c.cl_from";
-           // System.out.println(query_text);
-
+           
             ResultSet rs = st.executeQuery(query_text);
             
             while(rs.next()){
@@ -575,9 +550,7 @@ public class PathFinder {
         } catch (SQLException ex) {
             Logger.getLogger(PathFinder.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-    }
-    
+    }    
     
     private void saveRelatedPage(String pageTitle){
         System.out.println("Saving ... " + pageTitle);
@@ -589,16 +562,12 @@ public class PathFinder {
             
             st.executeUpdate(query);
             
-            
-         st.close();   
+            st.close();   
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(PathFinder.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            //Logger.getLogger(PathFinder.class.getName()).log(Level.SEVERE, , ex);
-            
+            //Logger.getLogger(PathFinder.class.getName()).log(Level.SEVERE, , ex);   
         }
-        
-        
     }
     
     private String getCategory(String path, String from, String to){
