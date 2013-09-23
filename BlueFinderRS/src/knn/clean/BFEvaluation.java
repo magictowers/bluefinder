2eeb7e8a47127b9bc01bbc4315d43d7bde191613
Marcia Kaminose <marcia.kaminose@lifia.info.unlp.edu.ar>
2013-09-23 14:20:52 -0300

    Realizar la evaluación para un único caso. Con UN test (asociado a un dump).

--- BlueFinderRS/src/knn/clean/BFEvaluation.java
+++ BlueFinderRS/src/knn/clean/BFEvaluation.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 import db.WikipediaConnector;
@@ -17,14 +18,40 @@ import strategies.LastCategoryGeneralization;
 public class BFEvaluation {
 	
 	private KNN knn;
-	
+	private int k;
+	private int maxRecomm;
+
 	public BFEvaluation() {}
 	
 	public BFEvaluation(KNN knn) {
 		this.knn = knn;
+		this.k = 5;
+		this.maxRecomm = 10000;
+	}
+	
+	public BFEvaluation(KNN knn, int k, int maxRecomm) {
+		this(knn);
+		this.k = k;
+		this.maxRecomm = maxRecomm;
+	}
+	
+	public int getK() {
+		return k;
 	}
 
-	public void getEvaluation(String object, String subject, int k, int maxRecomm) throws ClassNotFoundException, SQLException {
+	public void setK(int k) {
+		this.k = k;
+	}
+
+	public int getMaxRecomm() {
+		return maxRecomm;
+	}
+
+	public void setMaxRecomm(int maxRecomm) {
+		this.maxRecomm = maxRecomm;
+	}
+
+	public List<String> getEvaluation(String object, String subject) throws ClassNotFoundException, SQLException {
 		String relatedUFrom = "u_from=0 ";
 		String relatedString = "";
 
@@ -43,14 +70,9 @@ public class BFEvaluation {
 					+ relatedUFrom + ") group by v_to order by suma desc";
 			ResultSet paths = st.executeQuery(queryFixed);
 			TreeMap<String, Integer> map = this.genericPath(paths, knnResults.size() + 1);
-
 			knnResults.add(map.toString());
 		}
-		System.out.printf("Evaluation for the pair: %s , %s, k=%d, maxRecomm=%d\n", object, subject, k, maxRecomm);
-		if (maxRecomm < knnResults.size()) {
-			knnResults = knnResults.subList(0, maxRecomm);
-		}
-		System.out.println(knnResults);		
+		return knnResults;
 	}
 	
 	protected TreeMap<String, Integer> genericPath(ResultSet paths, int kValue) throws SQLException {
@@ -72,20 +94,30 @@ public class BFEvaluation {
 					&& !path.contains("All_pages_")
 					&& !path.contains("All_non")) {
 				if (pathDictionary.get(path) == null) {
-					if (suma == kValue) { // all the cases belongs to this path
-											// query
+					if (suma == kValue) { // all the cases belongs to this path query
 						suma = suma + 1000;
 					}
-					pathDictionary.put(path, suma);
 				} else {
 					suma += pathDictionary.get(path);
-					pathDictionary.put(path, suma);
 				}
+				pathDictionary.put(path, suma);
 			}
 		}
+		
 		for (String path : pathDictionary.keySet()) {
 			Integer suma = pathDictionary.get(path);
 			sortedMap.put(path, suma);
+		}
+		
+		if (sortedMap.size() > this.maxRecomm) {
+			int cantPaths = 0;
+			TreeMap<String, Integer> tmpSortedMap = new TreeMap<String, Integer>(bvc);
+			while (cantPaths < this.maxRecomm) {
+				Map.Entry<String, Integer> entry = sortedMap.pollFirstEntry();
+				tmpSortedMap.put(entry.getKey(), entry.getValue());
+				cantPaths++;
+			}
+			sortedMap = tmpSortedMap;
 		}
 		return sortedMap;
 	}
@@ -95,18 +127,18 @@ public class BFEvaluation {
 			System.out.println("Expected arguments: <from> <to> <neighbour> [<max recommendations>]");
 			System.exit(255);
 		}
-		String object = args[0];
-//		object = "Bloodletting_(The_Walking_Dead)";
-		String subject = args[1];
-//		subject = "Maggie_Greene";
+		String subject = args[0];
+		String object = args[1];
 		int k = 5;
 		try {
 			k = Integer.parseInt(args[2]);
+			System.out.println(k);
 			if (k > 10) {
 				throw new NumberFormatException();
 			}
-		} catch (NumberFormatException ex) {
+		} catch (NumberFormatException ex) {			
 			System.err.println("Invalid neighbour, set to default (5).");
+			k = 5;
 		}
 		int maxRecomm = 100000;
 		try {
@@ -116,8 +148,13 @@ public class BFEvaluation {
 		} catch (ArrayIndexOutOfBoundsException ex) {
 			System.err.println("Number of recommendations was not provided, set to default (all).");
 		}
-		BFEvaluation bfevaluation = new BFEvaluation(new KNN(false));
-		bfevaluation.getEvaluation(object,  subject,  k, maxRecomm);
+		BFEvaluation bfevaluation = new BFEvaluation(new KNN(false), k, 1);
+		List<String> knnResults = bfevaluation.getEvaluation(object,  subject);
+
+		System.out.printf("Evaluation for the pair: %s , %s, k=%d, maxRecomm=%d\n", object, subject, k, maxRecomm);
+		for (int i = 0; i < knnResults.size(); i++) {
+			System.out.println((i + 1) + " -> " + knnResults.get(i));
+		}
 	}
 
 }
---
+++ BlueFinderRS/test/knn/clean/BFEvaluationTest.java
@@ -0,0 +1,179 @@
+package knn.clean;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import db.WikipediaConnector;
+
+public class BFEvaluationTest {
+
+	private BFEvaluation bfEvaluation;
+	
+	@BeforeClass
+	public static void setupclass(){
+		Assume.assumeTrue(WikipediaConnector.isTestEnvironment());
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		KNN knn = new KNN(false);
+		this.bfEvaluation = new BFEvaluation(knn);
+	}
+
+	@Test
+	public void testGetEvaluation() {
+		String subject = "France";
+		String object = "William_Kissam_Vanderbilt";
+		this.bfEvaluation.setK(6);
+		this.bfEvaluation.setMaxRecomm(3);
+		try {
+			List<String> actualResult = this.bfEvaluation.getEvaluation(object, subject);
+			List<String> expectedResult = new ArrayList<String>();
+			Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+			
+			map.put("#from / * / Cat:French_businesspeople / #to", 1015);
+			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 1001);
+			expectedResult.add(map.toString());
+			
+			map.clear();
+			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 1002);
+			map.put("#from / * / Cat:French_businesspeople / #to", 15);
+			expectedResult.add(map.toString());
+			
+			map.clear();
+			map.put("#from / * / Cat:French_businesspeople / #to", 15);
+			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 2);
+			map.put("#from / * / Cat:Drug-related_deaths_in_#from / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear();
+			map.put("#from / * / Cat:French_businesspeople / #to", 15);
+			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 2);
+			map.put("#from / * / Cat:Kingdom_of_#from_stubs / #to", 2);
+			expectedResult.add(map.toString());
+			
+			map.clear();
+			map.put("#from / * / Cat:French_businesspeople / #to", 15);
+			map.put("#from / * / Cat:Ephrussi_family / #to", 7);
+			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 3);
+			expectedResult.add(map.toString());
+			
+			assertEquals("Puede que las evaluaciones sean iguales, pero en diferente orden si la cantida de apariciones son iguales", 
+					expectedResult, actualResult);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			fail("ClassNotFoundException");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			fail("SQLException");
+		}
+	}
+	
+	@Test
+	public void testGetEvaluation2() {
+		String subject = "New_York_City";
+		String object = "William_Kissam_Vanderbilt_II";
+		this.bfEvaluation.setK(10);
+		this.bfEvaluation.setMaxRecomm(10000);
+		try {
+			List<String> actualResult = this.bfEvaluation.getEvaluation(object, subject);
+			List<String> expectedResult = new ArrayList<String>();
+			Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+			
+			// 1k
+			map.put("#from / * / Cat:#from_Ballet / #to", 1001);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 2k
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 3k
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 4k
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			map.put("#from / * / Cat:Good_articles / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 5k
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			map.put("#from / * / Cat:Good_articles / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 6k
+			map.put("#from / * / Cat:History_of_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			map.put("#from / * / Cat:Good_articles / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 7k
+			map.put("#from / * / Cat:History_of_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:Architects_from_#from / #to", 3);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			map.put("#from / * / Cat:Artists_from_Philadelphia,_Pennsylvania / #to", 1);
+			map.put("#from / * / Cat:Good_articles / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 8k
+			map.put("#from / * / Cat:History_of_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:Architects_from_#from / #to", 3);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			map.put("#from / * / Cat:Artists_from_Philadelphia,_Pennsylvania / #to", 1);
+			map.put("#from / * / Cat:Good_articles / #to", 1);
+			map.put("#from / * / Cat:American_clowns / #to", 1);
+			expectedResult.add(map.toString());
+			
+			map.clear(); // 9k
+			map.put("#from / * / Cat:History_of_#from / #to", 4);
+			map.put("#from / * / Cat:People_from_#from / #to", 4);
+			map.put("#from / * / Cat:Architects_from_#from / #to", 3);
+			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
+			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
+			map.put("#from / * / Cat:#from_Ballet / #to", 1);
+			map.put("#from / * / Cat:The_King's_College_(New_York) / #to", 1);
+			map.put("#from / * / Cat:Artists_from_Philadelphia,_Pennsylvania / #to", 1);
+			map.put("#from / * / Cat:Good_articles / #to", 1);
+			map.put("#from / * / Cat:American_clowns / #to", 1);
+			expectedResult.add(map.toString());
+			
+			assertEquals("Puede que las evaluaciones sean iguales, pero en diferente orden si la cantida de apariciones son iguales", 
+					expectedResult, actualResult);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			fail("ClassNotFoundException");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			fail("SQLException");
+		}
+	}
+
+}
