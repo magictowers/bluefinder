0d0a6d62d6251d4fe23d765f601b2b69d8eab37e
Marcia Kaminose <marcia.kaminose@lifia.info.edu.ar>
2014-02-17 14:10:29 -0300

    Booleano para crear o no tabla enhanced

--- BlueFinderRS/test/evals/EvaluationComparatorTest.java
+++ BlueFinderRS/test/evals/EvaluationComparatorTest.java
@@ -58,22 +58,39 @@ public class EvaluationComparatorTest {
     // @Test
     // public void hello() {}
     
+    /**
+     * Evaluate to true, they both tuples contain only one path, the direct one.
+     */
     @Test
-    public void testGetConventions() {
+    public void testGetConventions1() {
         FromToPair pairEn = new FromToPair("Abadía_de_Claraval", "Bernardo_de_Claraval", "en");
         FromToPair pairEs = new FromToPair("Abadía_de_Fontevrault", "Isabel_de_Angulema", "es");
         Set<String> expected = new HashSet<String>();
         expected.add(FromToPair.FROM_WILDCARD + PathsResolver.STEP_SEPARATOR + FromToPair.TO_WILDCARD);
-        expected.add(FromToPair.FROM_WILDCARD + PathsResolver.STEP_SEPARATOR + 
-                PathsResolver.CATEGORY_PREFIX + FromToPair.FROM_WILDCARD +
-                PathsResolver.STEP_SEPARATOR + FromToPair.TO_WILDCARD);
-        expected.add(FromToPair.FROM_WILDCARD + PathsResolver.STEP_SEPARATOR + 
-                PathsResolver.CATEGORY_PREFIX + "adsfasdfasdf" + PathsResolver.STEP_SEPARATOR + FromToPair.TO_WILDCARD);
         Set<String> actual = this.evalComparator.findConventions(pairEn, pairEs);
         assertEquals("No tienen la misma cantidad de convenciones", expected, actual);
         for (String strExpected : expected) {
             assertTrue("El path no se encuentra dentro de las convenciones.", actual.contains(strExpected));
-        }
-        
+        }        
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void testGetConventions2() {
+        FromToPair pairEn = new FromToPair("Alberta", "Alexander_Rutherford", "es");
+        FromToPair pairEs = new FromToPair("Alberta", "Ralph_Klein", "es");
+        Set<String> expected = new HashSet<String>();
+        expected.add(FromToPair.FROM_WILDCARD + PathsResolver.STEP_SEPARATOR + "*" + PathsResolver.STEP_SEPARATOR +
+                PathsResolver.CATEGORY_PREFIX + "Anglo-albertanos" + PathsResolver.STEP_SEPARATOR + FromToPair.TO_WILDCARD);
+        expected.add(FromToPair.FROM_WILDCARD + PathsResolver.STEP_SEPARATOR + "*" + PathsResolver.STEP_SEPARATOR +
+                PathsResolver.CATEGORY_PREFIX + "Premiers_of_" + FromToPair.FROM_WILDCARD + 
+                PathsResolver.STEP_SEPARATOR + FromToPair.TO_WILDCARD);
+        Set<String> actual = this.evalComparator.findConventions(pairEn, pairEs);
+        assertEquals("No tienen la misma cantidad de convenciones", expected, actual);
+        for (String strExpected : expected) {
+            assertTrue("El path no se encuentra dentro de las convenciones.", actual.contains(strExpected));
+        }        
     }
 }
