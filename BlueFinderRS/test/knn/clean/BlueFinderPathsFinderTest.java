58d1c343c78dba1bcddfcefe39447771ea027ebc
Marcia Kaminose <marcia.kaminose@lifia.info.edu.ar>
2014-04-23 15:08:35 -0300

    Fix test BlueFinderPathsFinderTest.

--- BlueFinderRS/test/knn/clean/BlueFinderPathsFinderTest.java
+++ BlueFinderRS/test/knn/clean/BlueFinderPathsFinderTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import db.WikipediaConnector;
 import db.utils.ResultsDbInterface;
 import org.junit.Assert;
+import utils.PathsResolver;
 
 public class BlueFinderPathsFinderTest {
 
@@ -54,44 +55,53 @@ public class BlueFinderPathsFinderTest {
 		String subject = "France";
 		String object = "William_Kissam_Vanderbilt";
         Integer id = -1; // this ID is supposed to be the tuple's
-		this.bfPathsFinder.setK(6);
+		this.bfPathsFinder.setK(6); //TODO: was this fixed???? check!
+        this.bfPathsFinder.setK(5);
 		this.bfPathsFinder.setMaxRecomm(3);
 		try {
-			List<String> actualResult = this.bfPathsFinder.getEvaluation(object, subject, id);
-			List<String> expectedResult = new ArrayList<String>();
+			List<String> actualStrResult = this.bfPathsFinder.getEvaluation(object, subject, id);
+            List<Map<String, Integer>> expected = new ArrayList<Map<String, Integer>>();
 			Map<String, Integer> map = new LinkedHashMap<String, Integer>();
 			
 			map.put("#from / * / Cat:French_businesspeople / #to", 1015);
 			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 1001);
-			expectedResult.add(map.toString());
+            expected.add(map);
 			
 			map.clear();
 			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 1002);
 			map.put("#from / * / Cat:French_businesspeople / #to", 15);
-			expectedResult.add(map.toString());
+            expected.add(map);
 			
 			map.clear();
 			map.put("#from / * / Cat:French_businesspeople / #to", 15);
 			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 2);
 			map.put("#from / * / Cat:Drug-related_deaths_in_#from / #to", 1);
-			expectedResult.add(map.toString());
+            expected.add(map);
 			
 			map.clear();
 			map.put("#from / * / Cat:French_businesspeople / #to", 15);
 			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 2);
 			map.put("#from / * / Cat:Kingdom_of_#from_stubs / #to", 2);
-			expectedResult.add(map.toString());
+            expected.add(map);
 			
 			map.clear();
 			map.put("#from / * / Cat:French_businesspeople / #to", 15);
 			map.put("#from / * / Cat:Ephrussi_family / #to", 7);
 			map.put("#from / * / Cat:French_racehorse_owners_and_breeders / #to", 3);
-			expectedResult.add(map.toString());
+            expected.add(map);
 
-            Assert.assertFalse("No puede no dar recomendaciones", expectedResult.isEmpty());
-			assertEquals("No tienen la misma cantidad de recomendaciones.", expectedResult.size(), actualResult.size());
-			assertEquals("Puede que las evaluaciones sean iguales, pero en diferente orden si la cantidad de apariciones son iguales", 
-					expectedResult, actualResult);
+            PathsResolver pathsResolver = new PathsResolver();
+            List<Map<String, Integer>> actual = new ArrayList<Map<String, Integer>>();
+            for (String str : actualStrResult) {
+                actual.add(pathsResolver.decouple(str));
+            }
+            
+            Assert.assertFalse("No puede no dar recomendaciones", actual.isEmpty());
+			assertEquals("No tienen la misma cantidad de recomendaciones.", expected.size(), actual.size());
+            
+            for (Map<String, Integer> inner : expected) {
+                Assert.assertTrue("No tienen los mismos elementos", actual.contains(inner));
+            }
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			fail("ClassNotFoundException");
@@ -106,34 +116,35 @@ public class BlueFinderPathsFinderTest {
 		String subject = "New_York_City";
 		String object = "William_Kissam_Vanderbilt_II";
         Integer id = -1;
-		this.bfPathsFinder.setK(10);
+		this.bfPathsFinder.setK(10); //TODO: was this fixed???? check!
+        this.bfPathsFinder.setK(9);
 		this.bfPathsFinder.setMaxRecomm(10000);
 		try {
-			List<String> actualResult = this.bfPathsFinder.getEvaluation(object, subject, id);
-			List<String> expectedResult = new ArrayList<String>();
+			List<String> actualStrResult = this.bfPathsFinder.getEvaluation(object, subject, id);
+            List<Map<String, Integer>> expected = new ArrayList<Map<String, Integer>>();
 			Map<String, Integer> map = new LinkedHashMap<String, Integer>();
 			
 			// 1k
 			map.put("#from / * / Cat:#from_Ballet / #to", 1001);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 2k
 			map.put("#from / * / Cat:People_from_#from / #to", 4);
 			map.put("#from / * / Cat:#from_Ballet / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 3k
 			map.put("#from / * / Cat:People_from_#from / #to", 4);
 			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
 			map.put("#from / * / Cat:#from_Ballet / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 4k
 			map.put("#from / * / Cat:People_from_#from / #to", 4);
 			map.put("#from / * / Cat:People_from_Brooklyn / #to", 2);
 			map.put("#from / * / Cat:#from_Ballet / #to", 1);
 			map.put("#from / * / Cat:Good_articles / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 5k
 			map.put("#from / * / Cat:People_from_#from / #to", 4);
@@ -141,7 +152,7 @@ public class BlueFinderPathsFinderTest {
 			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
 			map.put("#from / * / Cat:#from_Ballet / #to", 1);
 			map.put("#from / * / Cat:Good_articles / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 6k
 			map.put("#from / * / Cat:History_of_#from / #to", 4);
@@ -150,7 +161,7 @@ public class BlueFinderPathsFinderTest {
 			map.put("#from / * / Cat:Shubert_Organization / #to", 1);
 			map.put("#from / * / Cat:#from_Ballet / #to", 1);
 			map.put("#from / * / Cat:Good_articles / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 7k
 			map.put("#from / * / Cat:History_of_#from / #to", 4);
@@ -161,7 +172,7 @@ public class BlueFinderPathsFinderTest {
 			map.put("#from / * / Cat:#from_Ballet / #to", 1);
 			map.put("#from / * / Cat:Artists_from_Philadelphia,_Pennsylvania / #to", 1);
 			map.put("#from / * / Cat:Good_articles / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 8k
 			map.put("#from / * / Cat:History_of_#from / #to", 4);
@@ -173,7 +184,7 @@ public class BlueFinderPathsFinderTest {
 			map.put("#from / * / Cat:Artists_from_Philadelphia,_Pennsylvania / #to", 1);
 			map.put("#from / * / Cat:Good_articles / #to", 1);
 			map.put("#from / * / Cat:American_clowns / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 			
 			map.clear(); // 9k
 			map.put("#from / * / Cat:History_of_#from / #to", 4);
@@ -186,12 +197,20 @@ public class BlueFinderPathsFinderTest {
 			map.put("#from / * / Cat:Artists_from_Philadelphia,_Pennsylvania / #to", 1);
 			map.put("#from / * / Cat:Good_articles / #to", 1);
 			map.put("#from / * / Cat:American_clowns / #to", 1);
-			expectedResult.add(map.toString());
+			expected.add(map);
 
-            Assert.assertFalse("No puede no dar recomendaciones", expectedResult.isEmpty());
-			assertEquals("No tienen la misma cantidad de recomendaciones.", expectedResult.size(), actualResult.size());
-			assertEquals("Puede que las evaluaciones sean iguales, pero en diferente orden si la cantidad de apariciones son iguales", 
-					expectedResult, actualResult);
+            PathsResolver pathsResolver = new PathsResolver();
+            List<Map<String, Integer>> actual = new ArrayList<Map<String, Integer>>();
+            for (String str : actualStrResult) {
+                actual.add(pathsResolver.decouple(str));
+            }
+            
+            Assert.assertFalse("No puede no dar recomendaciones", actual.isEmpty());
+			assertEquals("No tienen la misma cantidad de recomendaciones.", expected.size(), actual.size());
+            
+            for (Map<String, Integer> inner : expected) {
+                Assert.assertTrue("No tienen los mismos elementos", actual.contains(inner));
+            }
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			fail("ClassNotFoundException");
@@ -206,7 +225,7 @@ public class BlueFinderPathsFinderTest {
 		String subject = "Washington,_D.C.";
 		String object = "Stephen_Johnson_Field";
 		Integer id = -1;
-		this.bfPathsFinder.setK(1);
+		this.bfPathsFinder.setK(2);
 		this.bfPathsFinder.setMaxRecomm(10000);
 		List<String> expectedResult = new ArrayList<String>();
 		Connection conn;
@@ -218,14 +237,14 @@ public class BlueFinderPathsFinderTest {
 			if (expectedDbResults.next()) {
 				expectedResult.add(expectedDbResults.getString("1path"));
 				expectedResult.add(expectedDbResults.getString("2path"));
-				expectedResult.add(expectedDbResults.getString("3path"));
-				expectedResult.add(expectedDbResults.getString("4path"));
-				expectedResult.add(expectedDbResults.getString("5path"));
-				expectedResult.add(expectedDbResults.getString("6path"));
-				expectedResult.add(expectedDbResults.getString("7path"));
-				expectedResult.add(expectedDbResults.getString("8path"));
-				expectedResult.add(expectedDbResults.getString("9path"));
-				expectedResult.add(expectedDbResults.getString("10path"));
+//				expectedResult.add(expectedDbResults.getString("3path"));
+//				expectedResult.add(expectedDbResults.getString("4path"));
+//				expectedResult.add(expectedDbResults.getString("5path"));
+//				expectedResult.add(expectedDbResults.getString("6path"));
+//				expectedResult.add(expectedDbResults.getString("7path"));
+//				expectedResult.add(expectedDbResults.getString("8path"));
+//				expectedResult.add(expectedDbResults.getString("9path"));
+//				expectedResult.add(expectedDbResults.getString("10path"));
 			}
 			List<String> actualResult = this.bfPathsFinder.getEvaluation(object, subject, id);
 			
@@ -238,46 +257,5 @@ public class BlueFinderPathsFinderTest {
 			fail("Check stack trace.");
 		}		
 	}
-	
-    /**
-     * Test for a table, not tuples
-     */
-    @Test
-    public void testGetEvaluation4() throws ClassNotFoundException, SQLException {
-        this.resultsDb.saveNotFoundPath("William_Kissam_Vanderbilt", "France");
-//        this.resultsDb.saveNotFoundPath("William_Kissam_Vanderbilt_II", "New_York_City");
-//        this.resultsDb.saveNotFoundPath("Stephen_Johnson_Field", "Washington,_D.C.");
-        
-        this.bfPathsFinder.setMaxRecomm(3);
-        this.bfPathsFinder.setSaveResults(true);
-        this.bfPathsFinder.getEvaluation("sc_testGetEvaluation4", 3, 3, 0);
-//        List<String> expectedResult = new ArrayList<String>();
-//		Connection conn;
-//		try {
-//			conn = WikipediaConnector.getTestConnection();
-//			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM sc3333 WHERE resource = ?");
-//			stmt.setString(1, subject + " , " + object + " 21156");
-//			ResultSet expectedDbResults = stmt.executeQuery();
-//			if (expectedDbResults.next()) {
-//				expectedResult.add(expectedDbResults.getString("1path"));
-//				expectedResult.add(expectedDbResults.getString("2path"));
-//				expectedResult.add(expectedDbResults.getString("3path"));
-//				expectedResult.add(expectedDbResults.getString("4path"));
-//				expectedResult.add(expectedDbResults.getString("5path"));
-//				expectedResult.add(expectedDbResults.getString("6path"));
-//				expectedResult.add(expectedDbResults.getString("7path"));
-//				expectedResult.add(expectedDbResults.getString("8path"));
-//				expectedResult.add(expectedDbResults.getString("9path"));
-//				expectedResult.add(expectedDbResults.getString("10path"));
-//			}
-//			List<String> actualResult = this.bfPathsFinder.getEvaluation(object, subject, id);
-//			assertEquals("No tienen la misma cantidad de recomendaciones.", expectedResult.size(), actualResult.size());
-//			assertEquals("Puede que las evaluaciones sean iguales, pero en diferente orden si la cantidad de apariciones son iguales", 
-//					expectedResult, actualResult);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			fail("Check stack trace.");
-//		}	
-    }
-    
+	 
 }
